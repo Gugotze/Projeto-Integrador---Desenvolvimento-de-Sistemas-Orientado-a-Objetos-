761e22f0078b27186db3bd4cc831acd0dfe220d9
Gustavo Fonseca <gustavofonsecap@hotmail.com>
2020-10-24 14:05:31 -0300

    Excluir conexao via banco de dados postgreeSQL

--- LojaDeRoupas/src/br/com/LojaDeRoupas/Util/ConexaoDB.java
+++ LojaDeRoupas/src/br/com/LojaDeRoupas/Util/ConexaoDB.java
@@ -6,30 +6,32 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConexaoDB {
-    
-    private static Connection connection = null;
-    
-    public static Connection getConnection() {
-        if (connection != null)
-            return connection;
-        else {
-            try {
-                Properties prop = new Properties();
-                
-                String user = "root";
-                String password = "root";
-                Class.forName("com.mysql.jdbc.Driver"); // Para quem for usar MySql
-               // Class.forName("org.postgresql.Driver");// Para quem for usar Postgres
-               connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/lojaderoupas", user, password);// Para quem for usar MySql
-                //connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/userdb",user, password);// Para quem for usar Postgres
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            return connection;
-        }
 
-    }
-    
+	private static Connection connection = null;
+
+	public static Connection getConnection() {
+		if (connection != null)
+			return connection;
+		else {
+			try {
+				Properties prop = new Properties();
+
+				String user = "root";
+				String password = "root";
+				Class.forName("com.mysql.jdbc.Driver");
+				connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/lojaderoupas", user, password);// Para
+										/* teste*/																				// quem
+																														// for
+																														// usar
+																														// MySql
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return connection;
+		}
+
+	}
+
 }
